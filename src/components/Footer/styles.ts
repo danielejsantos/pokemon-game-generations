d44b42189b267550f9dc335bce8c839fc7916fb1
Daniele Santos <js_dani@hotmail.com>
2020-10-09 15:30:19 -0300

    Create footer

---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 40px;
+  background-color: #e9e3e9;
+  bottom: 0;
+  position: fixed;
+
+  > span {
+    color: #696669;
+    font-size: 13px;
+  }
+`;
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import { Container } from './styles';
 
 import Header from '../Header';
+import Footer from '../Footer';
 
 const Layout: React.FC = () => {
   return (
     <Container>
       <Header />
+      <Footer />
     </Container>
   );
 }
