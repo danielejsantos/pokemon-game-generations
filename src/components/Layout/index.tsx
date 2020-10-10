cf462b6aa109b189b9c6f042ba9c06372c996d42
Daniele Santos <js_dani@hotmail.com>
2020-10-10 16:02:42 -0300

    Add main page to layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,12 +3,14 @@ import React from 'react';
 import { Container } from './styles';
 
 import Header from '../Header';
+import Main from '../Main';
 import Footer from '../Footer';
 
 const Layout: React.FC = () => {
   return (
     <Container>
       <Header />
+      <Main />
       <Footer />
     </Container>
   );
