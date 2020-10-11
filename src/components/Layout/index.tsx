38a7536637a904950f0b56d96c7d344a12e1c818
Daniele Santos <js_dani@hotmail.com>
2020-10-11 12:13:50 -0300

    Create exhibition logic

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Container } from './styles';
 
 import Header from '../Header';
 import Main from '../Main';
+import List from '../List';
+import Pokemon from '../Pokemon';
 import Footer from '../Footer';
 
 const Layout: React.FC = () => {
+  const [generationUrl, setGenerationUrl] = useState('');
+  const [pokemonUrl, setPokemonUrl] = useState('');
+
   return (
     <Container>
       <Header />
-      <Main />
+      {!generationUrl && <Main setGenerationUrl={setGenerationUrl} />}
+      {(generationUrl && !pokemonUrl) && <List generationUrl={generationUrl} setGenerationUrl={setGenerationUrl} setPokemonUrl={setPokemonUrl} />}
+      {(generationUrl && pokemonUrl) && <Pokemon pokemonUrl={pokemonUrl} setPokemonUrl={setPokemonUrl} />}
       <Footer />
     </Container>
   );
