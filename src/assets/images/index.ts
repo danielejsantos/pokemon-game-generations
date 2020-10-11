7128f6f0ebea698f1af30190e1f22468fc729929
Daniele Santos <js_dani@hotmail.com>
2020-10-11 12:06:54 -0300

    Export game generation images

---
+++ src/assets/images/index.ts
@@ -0,0 +1,19 @@
+import generationOneImg from "../../assets/images/generation-one.jpg";
+import generationTwoImg from "../../assets/images/generation-two.png";
+import generationThreeImg from "../../assets/images/generation-three.jpg";
+import generationFourImg from "../../assets/images/generation-four.jpg";
+import generationFiveImg from "../../assets/images/generation-five.jpg";
+import generationSixImg from "../../assets/images/generation-six.png";
+import generationSevenImg from "../../assets/images/generation-seven.png";
+import generationEightImg from "../../assets/images/generation-eight.jpg";
+
+export {
+  generationOneImg,
+  generationTwoImg,
+  generationThreeImg,
+  generationFourImg,
+  generationFiveImg,
+  generationSixImg,
+  generationSevenImg,
+  generationEightImg
+};
